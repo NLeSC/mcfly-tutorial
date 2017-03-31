c83d97a99020d04e4d3bfc8ef81ab8242104d5b5
dafnevk <d.vankuppevelt@esciencecenter.nl>
2017-03-31 10:44:53 +0200

    Script now also saves models

--- scripts/experiment_PAMAP2_9fold.py
+++ scripts/experiment_PAMAP2_9fold.py
@@ -17,13 +17,22 @@ import pandas as pd
 from mcfly import modelgen, find_architecture, storage
 
 
+# In[11]:
+
+trainsize = 500
+valsize = 100 
+nr_models = 10
+nr_epochs = 10
+subset_size = trainsize
+
+
 # ## Load the data
 
 # In[2]:
 
 # Define directory where the results, e.g. json file, will be stored
-datapath = '/data/mcfly/input/'
-resultpath = '/data/mcfly/output/' 
+datapath = '/data/mcfly/input'
+resultpath = '/data/mcfly/output' 
 if not os.path.exists(resultpath):
         os.makedirs(resultpath)
 
@@ -48,19 +57,30 @@ print(Xs[0].shape, ys[0].shape)
 
 # First step is to create a model architecture. As we do not know what architecture is best for our data we will create a set of models to investigate which architecture is most suitable for our data and classification task. You will need to specificy how many models you want to create with argument 'number_of_models', the type of model which can been 'CNN' or 'DeepConvLSTM', and maximum number of layers per modeltype. See for a full overview of the optional arguments the function documentation of modelgen.generate_models
 
-# In[5]:
+# In[16]:
 
 num_classes = ys[0].shape[1]
 np.random.seed(123)
 models = modelgen.generate_models(Xs[0].shape,
                                   number_of_classes=num_classes,
-                                  number_of_models = 15)
+                                  number_of_models = nr_models)
+
+
+# In[ ]:
+
+
+
+
+# In[19]:
+
+for i, (model, params, model_type) in enumerate(models):
+    storage.savemodel(model,resultpath,"model_"+str(i))
 
 
 # ## Compare models
 # Now that the model architectures have been generated it is time to compare the models by training them in a subset of the training data and evaluating the models in the validation subset. This will help us to choose the best candidate model. Performance results are stored in a json file.
 
-# In[6]:
+# In[25]:
 
 def split_train_test(X_list, y_list, j):
     X_train = np.concatenate(X_list[0:j]+X_list[j+1:])
@@ -80,7 +100,7 @@ def split_train_small_val(X_list, y_list, j, trainsize=500, valsize=500):
     return X_train, y_train, X_val, y_val
 
 
-# In[9]:
+# In[26]:
 
 from keras.optimizers import Adam
 from keras.models import model_from_json
@@ -101,12 +121,6 @@ def get_fresh_copy(model, lr):
 models = [(get_fresh_copy(model, params['learning_rate']), params, model_type)  for model, params, model_type in models]
 
 
-# In[11]:
-
-trainsize = 500
-valsize = 500 
-
-
 # In[12]:
 
 import time
@@ -120,8 +134,8 @@ for j in range(len(Xs)):
     histories, val_accuracies, val_losses = find_architecture.train_models_on_samples(X_train, y_train,
                                                                            X_val, y_val,
                                                                            models,
-                                                                           nr_epochs=10,
-                                                                           subset_size=500,
+                                                                           nr_epochs=nr_epochs,
+                                                                           subset_size=subset_size,
                                                                            verbose=True,
                                                                            outputfile=os.path.join(resultpath, 
                                                                                   'experiment'+str(j)+'.json'),
@@ -132,22 +146,22 @@ for j in range(len(Xs)):
 print(time.time()-t)
 
 
-# In[13]:
+# In[6]:
 
 # Read them all back in
 import json
 model_jsons = []
-for j in range(len(Xs)):
+for j in len(Xs):
     with open(os.path.join(resultpath, 'experiment'+str(j)+'.json'), 'r') as outfile:
         model_jsons.append(json.load(outfile))
 
 
-# In[ ]:
+# In[12]:
 
 val_accuracies = np.array([[mod['val_acc'][-1] for mod in fold] for fold in model_jsons])
 
 
-# In[ ]:
+# In[13]:
 
 val_acc = np.array([np.array([mod['val_acc'][-1] for mod in fold], dtype='float') for fold in model_jsons])
 train_acc = np.array([np.array([mod['train_acc'][-1] for mod in fold], dtype='float') for fold in model_jsons])
@@ -155,25 +169,21 @@ train_loss = np.array([np.array([mod['train_loss'][-1] for mod in fold], dtype='
 val_loss = np.array([np.array([mod['val_loss'][-1] for mod in fold], dtype='float') for fold in model_jsons])
 
 
-# In[ ]:
+# In[14]:
 
 val_accuracies_avg = val_acc.mean(axis=0)
 print('val_accuracies_avg:', val_accuracies_avg)
 
 
-# In[ ]:
+# In[23]:
 
 best_model_index = np.argmax(val_accuracies_avg)
-best_model, best_params, best_model_types = models[best_model_index]
-print('Model type and parameters of the best model:')
-print(best_model_types)
-print(best_params)
+best_model = storage.loadmodel(resultpath, 'model_'+str(best_model_index))
 
 
-# In[ ]:
+# In[28]:
 
-modelname = 'bestmodel_sample'
-storage.savemodel(best_model,resultpath,modelname)
+best_params = model_jsons[0][best_model_index]
 
 
 # ## Train the best model for real
@@ -201,20 +211,6 @@ for j in range(len(Xs)):
 
 # In[ ]:
 
-print('accuracies: ', test_accuracies_list)
-
-
-# In[ ]:
-
-print(np.mean(test_accuracies_list))
-
-
-# ### Saving, loading and comparing reloaded model with orignal model
-
-# The modoel can be saved for future use. The savemodel function will save two separate files: a json file for the architecture and a npy (numpy array) file for the weights.
-
-# In[ ]:
-
 modelname = 'my_bestmodel'
 
 
@@ -222,6 +218,16 @@ modelname = 'my_bestmodel'
 
 for i, model in enumerate(models):
     storage.savemodel(model,resultpath,modelname+str(i))
+
+
+# In[ ]:
+
+print('accuracies: ', test_accuracies_list)
+
+
+# In[ ]:
+
+print(np.mean(test_accuracies_list))
 
 
 # In[ ]:
